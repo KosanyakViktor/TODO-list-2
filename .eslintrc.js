b57bdb696475e34d33658c2a5c775e3e1b15de5e
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-22 18:02:12 +0200

    commit-n

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.export = {
+    extends: 'eslint-config-airbnb-base',
+    rules: {
+        'no-console': 2,
+        'import/prefer-default-export': 0,
+    },
+    env: {
+        browser: true,
+    }
+}
