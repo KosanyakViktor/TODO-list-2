82f97b5b70a7906a936d64a35f17e06e5ed2ca85
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-22 15:32:49 +0200

    kk

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,61 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                use: ['babel-loader']
-            },
-            {
-                test: /.s?css$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name].[ext]',
-                            limit: 8192,
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    const config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /.js$/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.s?css$/i,
+                    use: [
+                        isProduction 
+                          ? MiniCssExtractPlugin.loader 
+                          :'style-loader',
+                           'css-loader', 
+                           'sass-loader'
+                        ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name].[ext]',
+                                limit: 8192,
+                            },
                         },
-                    },
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new webpack,ProgressPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        })
-    ]
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new webpack,ProgressPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+        ],
+        devServer: {
+            port: 9000,
+            hot: true,
+        }
+    };
+
+  if(isProduction) {
+      config.plugins.push(new MiniCssExtractPlugin({
+          filename: '[name].css',
+      }));
+  }
+  return config;
 };
